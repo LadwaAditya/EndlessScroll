a481a85452ffb3fbe3e4ee893e38daf6445ce9ef
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-03-25 20:38:16 +0530

    fect(Main): Movie Click Listener

--- app/src/main/java/com/ladwa/aditya/moviesearch/ui/adapter/MovieListAdapter.java
+++ app/src/main/java/com/ladwa/aditya/moviesearch/ui/adapter/MovieListAdapter.java
@@ -1,11 +1,14 @@
 package com.ladwa.aditya.moviesearch.ui.adapter;
 
+import android.content.Context;
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -13,7 +16,6 @@ import com.ladwa.aditya.moviesearch.R;
 import com.ladwa.aditya.moviesearch.data.model.MovieResponse;
 import com.ladwa.aditya.moviesearch.databinding.ListItemMovieBinding;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -46,16 +48,19 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private ListItemMovieBinding binding;
+        private ListItemMovieBinding mBinding;
+        private MovieResponse.Movie mMovie;
 
         public ViewHolder(ListItemMovieBinding binding) {
             super(binding.getRoot());
-            this.binding = binding;
+            this.mBinding = binding;
+            binding.setHolder(this);
         }
 
         public void bindMovie(MovieResponse.Movie movie) {
-            binding.setMovie(movie);
-            binding.executePendingBindings();
+            mMovie = movie;
+            mBinding.setMovie(movie);
+            mBinding.executePendingBindings();
         }
 
         @BindingAdapter({"bind:imageUrl"}) public static void loadImage(ImageView imageView, String imageUrl) {
@@ -64,6 +69,11 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.View
                     .placeholder(R.color.colorAccent)
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .into(imageView);
+        }
+
+        public void onClick(View view) {
+            Context context = view.getContext();
+            Toast.makeText(context, mMovie.getTitle(), Toast.LENGTH_SHORT).show();
         }
     }
 }
